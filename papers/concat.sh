fbafa507f3ad0133f3980fedfceb9ed6146a33a7
abhijin <aa5ts@virginia.edu>
2024-02-09 13:32:56 -0500

    papers

---
+++ papers/concat.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+pdftk \
+    de2021thermal.pdf \
+    chen2021prioritizing.pdf \
+    adiga2020boolean.pdf \
+    cedeno2020data.pdf \
+    adiga2020bounds.pdf \
+    adiga2019validating.pdf \
+    hu2019modeling.pdf \
+    maharjan2019predicting.pdf \
+    mcnitt2019assessing.pdf \
+    adiga2019pac.pdf \
+    venkatramanan2020modeling.pdf \
+    cedeno2019mechanistic.pdf \
+    adiga2019graphical.pdf \
+    adiga2018inferringprob.pdf \
+    ren2018generative.pdf \
+    adiga2018sublinear.pdf \
+    adiga2018disparities.pdf \
+    heinrichs2018pest.pdf \
+    adiga2018learning.pdf \
+    adiga2017kserver.pdf \
+    venkat2017towards.pdf \
+    campos2017western.pdf \
+    adiga2017activity.pdf \
+    adiga2017inferring.pdf \
+    zhang2016near.pdf \
+    adiga2016delay.pdf \
+    adiga2016temporal.pdf \
+    cat output publications.pdf
+    
