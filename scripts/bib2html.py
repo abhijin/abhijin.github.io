01f12edbeda57a789873c037d841ac423af2b394
abhijin <aa5ts@virginia.edu>
2025-02-11 22:20:35 -0500

    papers done

--- scripts/bib2html.py
+++ scripts/bib2html.py
@@ -2,6 +2,7 @@ import bibtexparser
 import numpy as np
 import pandas as pd
 from pdb import set_trace
+from re import sub
 
 BIB = '../cv/mypapers.bib'
 HTML = 'papers.html'
@@ -12,33 +13,42 @@ def bib_html(row, file=None):
     elif row.journal != -1:
         venue = row.journal
     else:
-        raise ValueError(row.ID, 'Could not extract venue.')
+        print(row.ID, 'Could not extract venue.')
+        return
+    
+    row.title = sub('\n', ' ', row.title)
+    row.author = sub('\n', ' ', row.author)
+    venue = sub('\n', ' ', venue)
+
     str = f'''
-<li><p>
-    {row.title}<br>
-    {row.author}<br>
-    {venue}, 2021.<br>
-'''
+    <li><p>
+        {row.title}<br>
+        {row.author}<br>
+        {venue}, {row.year}.<br>
+    '''
+
     if row.url != -1:
         url = row.url
         str += f'''
-    <a href="{url}" class="btn btn-outline-dark btn-xs">PDF</a>
+        <a href="{url}" class="btn btn-outline-dark btn-xs">PDF</a>
 '''
 
     str += '''
-</p></li>
+    </p></li>
 '''
     f.write(str)
+    return
 
 
 with open(BIB, 'r') as f:
     bib = bibtexparser.load(f)
 
 df = pd.DataFrame(bib.entries)
-set_trace()
 df = df.sort_values('year', ascending=False)
 df = df.fillna(-1)
 
 with open(HTML, 'w') as f:
+    f.write('<ul>')
     df.apply(bib_html, axis=1, file=f)
+    f.write('</ul>')
 
