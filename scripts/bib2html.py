8829afbddeb6fb7a961460dc4047445ba0a7b714
abhijin <aa5ts@virginia.edu>
2025-04-18 08:12:32 -0400

    more of bio

--- scripts/bib2html.py
+++ scripts/bib2html.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from pdb import set_trace
 from re import sub
+from scholarly import scholarly
 
 BIB = '../cv/mypapers.bib'
 HTML = 'papers.html'
@@ -39,13 +40,47 @@ def bib_html(row, file=None):
     f.write(str)
     return
 
+def authors(row):
+    new_auth_list = []
+    for auth in row.split(' and '):
+        name_disagg = auth.split(', ')
+        try:
+            new_auth_list.append(name_disagg[1] + ' ' + name_disagg[0])
+        except IndexError:
+            if name_disagg[0] == 'others':
+                new_auth_list.append(name_disagg[0])
+    try:
+        return ', '.join(new_auth_list[0:-1]) + ' and ' + new_auth_list[-1]
+    except:
+        set_trace()
 
-with open(BIB, 'r') as f:
-    bib = bibtexparser.load(f)
+# Search for your author profile
+search_query = scholarly.search_author("Abhijin Adiga University of Virginia")
+author = next(search_query)
 
-df = pd.DataFrame(bib.entries)
+# Fill in details (including publications & citations)
+author = scholarly.fill(author, sections=["basics", "indices", "publications"])
+
+publist = []
+for i, pub in enumerate(author["publications"], start=1):
+    print(i, '/', len(author['publications']))
+    filled_pub = scholarly.fill(pub)
+    publist.append({
+        'title': filled_pub["bib"].get("title", "No Title"),
+        'year': filled_pub["bib"].get("pub_year", -1),
+        'citation': filled_pub["bib"].get("citation", ""),
+        'author': filled_pub["bib"].get("author", ""),
+        'id': int(filled_pub.get('cites_id')[0])})
+    if i >= 5:
+        break
+
+df = pd.DataFrame(publist)
+set_trace()
 df = df.sort_values('year', ascending=False)
 df = df.fillna(-1)
+df['author_'] = df.author
+df.author = df.author.apply(authors)
+set_trace()
 
 with open(HTML, 'w') as f:
     f.write('<ul>')
